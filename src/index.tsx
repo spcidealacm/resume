7844341bea7faf45e0c9a09ea73d0ffcc831fb06
spcidealacm <spcidealacm@gmail.com>
2021-04-19 16:53:30 +1000

    delete hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HashRouter> */}
     <App />
-    {/* </HashRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
