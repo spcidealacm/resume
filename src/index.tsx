ef7a57e075c5db2bd1f577d3b3d6c965db2cd4bf
spcidealacm <spcidealacm@gmail.com>
2021-04-19 16:22:15 +1000

    master domin

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    {/* <HashRouter> */}
+    <App />
+    {/* </HashRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
