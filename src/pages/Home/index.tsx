b7c005ab0b4a9c5cd8746acb3e85ffb5b033839d
spcidealacm <spcidealacm@gmail.com>
2021-04-20 09:35:18 +1000

    pc or mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,12 @@ import Style from "./style.module.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import Parallax from "parallax-js";
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
 import imgLinkedin from "../../assets/images/linkedin.png";
 import imgFacebook from "../../assets/images/facebook.png";
 import imgInstagram from "../../assets/images/instagram.png";
@@ -31,47 +37,60 @@ class Home extends React.Component<Props, State>{
         }
     }
     componentDidMount() {
-        const sceneOne = ReactDOM.findDOMNode(this.sceneOne.current);
-        new Parallax(sceneOne);
+        if (isBrowser) {
+            const sceneOne = ReactDOM.findDOMNode(this.sceneOne.current);
+            new Parallax(sceneOne);
+        } else if (isMobile) {
+
+        }
     }
 
     render() {
         return (
-            <div className={Style.home}>
-                <nav className={Style.nav}>
-                    <div className={Style.logo}>Explore.</div>
-                    <div className={Style.navlinks}>
-                        <ul>
-                            <li><img src={imgLinkedin} alt="linkedin logo" /></li>
-                            <li><img src={imgFacebook} alt="facebook logo" /></li>
-                            <li><img src={imgInstagram} alt="instagram logo" /></li>
-                        </ul>
+            <>
+                <BrowserView>
+                    <div className={Style.pcHome}>
+                        <nav className={Style.nav}>
+                            <div className={Style.logo}>Explore.</div>
+                            <div className={Style.navlinks}>
+                                <ul>
+                                    <li><img src={imgLinkedin} alt="linkedin logo" /></li>
+                                    <li><img src={imgFacebook} alt="facebook logo" /></li>
+                                    <li><img src={imgInstagram} alt="instagram logo" /></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <section className={Style.sectionOne}>
+                            <div ref={this.sceneOne}>
+                                <div data-depth="0.1" className={Style.bg}>
+                                    <img src={imgBackground} alt="background" />
+                                </div>
+                                <div data-depth="0.2" className={Style.rock}>
+                                    <img src={imgRock} alt="rock" />
+                                </div>
+                                <div data-depth="1.2" className={Style.earth}>
+                                    <img src={imgEarth} alt="earth" />
+                                </div>
+                                <div data-depth="0.1" className={Style.text}>
+                                    <h1>PLANETS</h1>
+                                </div>
+                                <div data-depth="0.4" className={Style.mid}>
+                                    <img src={imgMid} alt="mid" />
+                                </div>
+                                <div data-depth="0.1" className={Style.fore}>
+                                    <img src={imgFore} alt="foreground" />
+                                </div>
+                            </div>
+                        </section>
+                        <section className={Style.sectionTwo}></section>
                     </div>
-                </nav>
-                <section className={Style.sectionOne}>
-                    <div ref={this.sceneOne}>
-                        <div data-depth="0.1" className={Style.bg}>
-                            <img src={imgBackground} alt="background" />
-                        </div>
-                        <div data-depth="0.2" className={Style.rock}>
-                            <img src={imgRock} alt="rock" />
-                        </div>
-                        <div data-depth="1.2" className={Style.earth}>
-                            <img src={imgEarth} alt="earth" />
-                        </div>
-                        <div data-depth="0.1" className={Style.text}>
-                            <h1>PLANETS</h1>
-                        </div>
-                        <div data-depth="0.4" className={Style.mid}>
-                            <img src={imgMid} alt="mid" />
-                        </div>
-                        <div data-depth="0.1" className={Style.fore}>
-                            <img src={imgFore} alt="foreground" />
-                        </div>
+                </BrowserView>
+                <MobileView>
+                    <div className={Style.mobileHome}>
+                        <h1 className={Style.title}> just a test </h1>
                     </div>
-                </section>
-                <section className={Style.sectionTwo}></section>
-            </div>
+                </MobileView>
+            </>
         )
     }
 }
